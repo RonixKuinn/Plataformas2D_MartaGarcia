9633d647f6f2990c44e9aa1f3acff72666716f33
RonixKuinn <154350005+RonixKuinn@users.noreply.github.com>
2024-09-20 10:22:43 +0200

    se mueve

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    private Rigidbody2D characterRigidbody;
+    private float horizontalInput;
+    [SerializeField]private float jumpForce = 5;
+    [SerializeField]private float characterSpeed = 4.5f;    // "[SerializeField]" es para que se vea en el inspector //la f solo se pone con decimales
+
+    void Awake()
+    {
+        characterRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        characterRigidbody.velocity = new Vector2(horizontalInput * characterSpeed, characterRigidbody.velocity.y);   // (1,x) = (lados,arriva) <-- [hay que añadir new] // tambies se puede poner directamente la dirección "right"
+    }
+
+
+}
