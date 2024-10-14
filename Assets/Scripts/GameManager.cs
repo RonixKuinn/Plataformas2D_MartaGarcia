9db3ba1affcb9e9cef059299a6db8f963cfff61b
RonixKuinn <154350005+RonixKuinn@users.noreply.github.com>
2024-10-14 13:33:14 +0200

    suma_monedas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class GameManager : MonoBehaviour
     private int coins = 0;
     private bool isPaused;
     [SerializeField] GameObject pauseCanvas;
+    [SerializeField] Text coinText;
 
     void Awake()
     {
@@ -43,5 +45,6 @@ public class GameManager : MonoBehaviour
     public void AddCoin()
     {
         coins++;        //es lo mismo que "coin +=1;"
+        coinText.text = coins.ToString();
     }
 }
